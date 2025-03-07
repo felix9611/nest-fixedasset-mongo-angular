d68605dbb38508f07a0a0f0d05c263f7b9bd7415
Felix <53119101+felix9611@users.noreply.github.com>
2025-03-07 18:21:18 -0500

    add user info page

---
+++ frontend/src/app/component/page/userInfo/user-info.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
+import { UserInfo } from '../../../../state/interface'
+import { UserStoreService } from '../../../../state/user.service'
+import { NzFormModule } from 'ng-zorro-antd/form'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { FormsModule } from '@angular/forms'
+
+@Component({
+    // selector: 'app-footer',
+    imports: [CommonModule, RouterOutlet, RouterLink, FormsModule, RouterLinkActive, NzFormModule, NzInputModule],
+    templateUrl: './user-info.component.html',
+    styleUrl: './user-info.component.css',
+})
+export class UserInfoComponent implements OnInit {
+    constructor(private userService: UserStoreService) {}
+
+    userInfo: any = { 
+        _id: '',
+        username: '',
+        accessToken: '',
+        avatarBase64: '',
+        deptId: 0,
+        roleIds: [],
+        roleLists: [],
+        email: '', 
+    }
+
+    ngOnInit() {
+
+        this.userService.user$.subscribe(user => {
+            console.log(user)
+            this.userInfo = user
+            
+        })
+
+        
+    } 
+}
--- frontend/src/app/component/sub-router.ts
+++ frontend/src/app/component/sub-router.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router'
 import { AssetTypeComponent } from './page/asset/asset-type/asset-type.component'
 import { AuthGuard } from '../../state/AuthGuard'
+import { UserInfoComponent } from './page/userInfo/user-info.component'
 
 export const pagesRoutes: Routes = [
     {
         path: 'asset-type',
         component: AssetTypeComponent, 
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'user-info',
+        component: UserInfoComponent,
         canActivate: [AuthGuard]
     }
 ]
