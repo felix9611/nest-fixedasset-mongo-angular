6d4fbf84011876f41a8232ab2bc8103ba84409d4
Felix <53119101+felix9611@users.noreply.github.com>
2025-03-12 23:14:59 -0400

    update dept api doc

--- backend/src/module/department/department.controller.ts
+++ backend/src/module/department/department.controller.ts
@@ -1,42 +1,91 @@
 import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common'
 import { DepartmentService } from './department.service';
-import { CreateDeptDto, ListDeptRequestDto, UpdateDeptDto } from './department.dto';
-import { AuthGuard } from '../auth/AuthGuard';
+import { CreateDeptBody, DepartmentBody, ListDepartmentQuery, ListDepartmentQueryRes, ListDeptRequestDto, UpdateDeptBody, UpdateDeptDto } from './department.dto';
+import { AuthGuard } from '../auth/AuthGuard'
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger'
+import { ReturnMsg } from 'src/tool/open-api-body'
 
 @Controller('sys/department')
 export class DepartmentController {
     constructor(private deptService: DepartmentService){}
 
+    @ApiOperation({ summary: 'Create Department' })
+    @ApiBody({
+        type: CreateDeptBody
+    })
+    @ApiResponse({
+        description: 'If save successful',
+        status: 201,
+        type: DepartmentBody
+    })
+    @ApiResponse({
+        description: 'If not save successful',
+        status: 200,
+        type: ReturnMsg
+    })
     @Post('create')
     @UseGuards(AuthGuard)
     async create(@Body() createData: UpdateDeptDto) {
         return await this.deptService.create(createData)
     }
 
+    @ApiOperation({ summary: 'Update Department' })
+    @ApiBody({
+        type: UpdateDeptBody
+    })
+    @ApiResponse({
+        description: 'If not save successful',
+        status: 200,
+        type: ReturnMsg
+    })
     @Post('update')
     @UseGuards(AuthGuard)
     async update(@Body() updateDto: UpdateDeptDto) {
         return await this.deptService.update(updateDto)
     }
 
+    @ApiOperation({ summary: 'Get One by Id' })
+    @ApiResponse({
+        status: 201,
+        type: DepartmentBody
+    })
     @Get('one/:id')
     @UseGuards(AuthGuard)
     async getOneById(@Param('id') id: string) {
         return await this.deptService.getOneById(id)
     }
 
+    @ApiOperation({ summary: 'Void One by Id' })
+    @ApiResponse({
+            description: 'Return message only',
+            status: 200,
+            type: ReturnMsg
+    })
     @Get('remove/:id')
     @UseGuards(AuthGuard)
     async removeById(@Param('id') id: string) {
         return await this.deptService.invalidateDepartment(id)
     }
 
+    @ApiOperation({ summary: 'Get all data' })
+    @ApiResponse({
+        status: 201,
+        type: [DepartmentBody]
+    })
     @Get('getAll')
     @UseGuards(AuthGuard)
     async getAll() {
         return this.deptService.findAll()
     }
     
+    @ApiOperation({ summary: 'Lsit and page' })
+    @ApiBody({
+        type: ListDepartmentQuery
+    })
+    @ApiResponse({
+        status: 200,
+        type: ListDepartmentQueryRes
+})
     @Post('list')
     @UseGuards(AuthGuard)
     async listAndPage(@Body() req: ListDeptRequestDto) {
--- backend/src/module/department/department.dto.ts
+++ backend/src/module/department/department.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { CommonPageAndList, CommonPageAndListResponse } from 'src/tool/open-api-body'
+
 export interface CreateDeptDto {
     deptCode: string
     deptName: string,
@@ -12,4 +15,44 @@ export interface ListDeptRequestDto {
     page: number
     limit: number
     name?: string
+}
+
+export class CreateDeptBody {
+
+    @ApiProperty({ description: 'Department Code' })
+    deptCode: string
+
+    @ApiProperty({ description: 'Department Name' })
+    deptName: string
+
+    @ApiProperty({ description: 'Remark' })
+    remark: string
+}
+
+export class UpdateDeptBody extends CreateDeptBody {
+
+    @ApiProperty({ description: 'Data Id' })
+    _id: string
+}
+
+export class DepartmentBody extends UpdateDeptBody {
+
+    @ApiProperty({ description: 'Created At' })
+    createdAt: string
+
+    @ApiProperty({ description: 'Updated At' })
+    updatedAt: string
+
+    @ApiProperty({ description: '1 = Active, 0 = inactive' })  
+    status: number
+}
+
+export class ListDepartmentQuery extends CommonPageAndList {
+    @ApiProperty({ description: 'For search data keywords' })  
+    name: string
+}
+
+export class ListDepartmentQueryRes extends CommonPageAndListResponse {
+    @ApiProperty({ type: [DepartmentBody], description: 'Data List' })
+    lists: DepartmentBody[]
 }
