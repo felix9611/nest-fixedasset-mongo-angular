11c88967dd8d709b23cd08c47495cd440a4c2ac7
Felix <53119101+felix9611@users.noreply.github.com>
2025-03-13 23:21:59 -0400

    add to module

--- backend/src/module/sys-menu/sys-menu.module.ts
+++ backend/src/module/sys-menu/sys-menu.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { SysMenu, SysMenuSchema } from './sys-menu.schame'
+import { SysMenuService } from './sys-menu.service'
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: SysMenu.name, schema: SysMenuSchema }
         ])
-    ]
+    ],
+    providers: [SysMenuService]
 })
 export class SysMenuMoudule {}
