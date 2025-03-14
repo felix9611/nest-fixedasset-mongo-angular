c6b867debce1d156db5060cd46325df0ace1d484
Felix <53119101+felix9611@users.noreply.github.com>
2025-03-13 23:21:04 -0400

    update sys menu

--- backend/src/module/sys-menu/sys-menu.service.ts
+++ backend/src/module/sys-menu/sys-menu.service.ts
@@ -1,4 +1,57 @@
+import { Injectable } from '@nestjs/common'
+import { Model } from 'mongoose'
+import { SysMenu } from './sys-menu.schame'
+import { InjectModel } from '@nestjs/mongoose'
 
+
+@Injectable()
+export class SysMenuService {
+  constructor(
+    @InjectModel(SysMenu.name) private sysMenuModel: Model<SysMenu>
+  ) {}
+
+  async getTreeAllMenu() {
+    const items = await this.sysMenuModel.find({ status: 1 }).exec()
+
+    const final = this.nestItems(items)
+
+    return final
+  }
+
+  nestItems(items: any[]) {
+    const map = new Map<number, any>(); // Store items by ID
+    const roots: any[] = []; // Store top-level items
+  
+    // First, populate the map
+    items.forEach(item => map.set(item.id, { ...item, children: [] }))
+  
+    // Then, build the hierarchy
+    items.forEach(item => {
+      if (item.mainId === 0) {
+        roots.push(map.get(item._id)!)
+      } else {
+        const parent = map.get(item.mainId)
+        if (parent) {
+          parent.children!.push(map.get(item._id)!)
+        }
+      }
+    })
+  
+    this.sortItems(roots)
+    return roots
+  }
+
+  sortItems(items: any[]) {
+    items.sort((a, b) => a.sort - b.sort)
+    items.forEach(item => {
+      if (item.childrens!.length > 0) {
+        this.sortItems(item.childrens!)
+      } else {
+        delete item.childrens // Remove empty children arrays
+      }
+    })
+  }
+}
 /*
 
 type Item = {
