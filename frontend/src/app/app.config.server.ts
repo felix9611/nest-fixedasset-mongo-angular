6930d984d2ea8acbd9c36b9aaeddd37adeb613df
Felix <53119101+felix9611@users.noreply.github.com>
2026-09-24 21:40:50 -0400

    upgrade to angular  SSR V21

--- frontend/src/app/app.config.server.ts
+++ frontend/src/app/app.config.server.ts
@@ -1,14 +1,15 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
-import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core'
+import { provideServerRendering, withRoutes} from '@angular/ssr'
+import { appConfig } from './app.config'
+import { serverRoutes } from './app.routes.server'
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(),
-    provideServerRouting(serverRoutes)
+    provideServerRendering(
+      withRoutes(serverRoutes)
+    )
   ]
-};
+}
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+export const config =
+  mergeApplicationConfig(appConfig, serverConfig)
